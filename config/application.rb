59f0fe568982eb3ae243da7a4c09881c6be40430
Igor Marques da Silva <igormarquessilva@gmail.com>
2014-05-10 11:17:41 -0300

    Update date show for brazilian format

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module DoacoEs
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    Date::DATE_FORMATS.merge!(:default => "%d/%m/%C%y")
   end
 end
